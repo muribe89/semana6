6d80df84f1cf80aac20c87690840fe3bfb971498
muribe89 <90163000+muribe89@users.noreply.github.com>
2021-09-06 05:55:00 +0200

    Update TestNgLogin_incorrecto.java

--- src/test/java/TestNG/TestNgLogin_incorrecto.java
+++ src/test/java/TestNG/TestNgLogin_incorrecto.java
@@ -20,8 +20,8 @@ public class TestNgLogin_incorrecto {
 		  }
 	  @Test
 	  public void login() {
-			System.out.println("Este paso ingresa el nombre de usuario correcto y contraseña incorrecta en la pagina de login");
-			driver.findElement(By.id("username")).sendKeys("roly");
+			System.out.println("Este paso ingresa el nombre de usuario correcto y contraseÃ±a incorrecta en la pagina de login");
+			driver.findElement(By.id("username")).sendKeys("MURIBE");
 			driver.findElement(By.id("password")).sendKeys("12345");
 	  }
 	  
